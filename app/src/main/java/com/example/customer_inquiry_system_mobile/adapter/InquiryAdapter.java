eedb32aa4f8f0d6af4d70d59ed2cf95008c4899c
born-A <ra_ari@naver.com>
2024-09-02 17:13:43 +0900

    pbs-321--feat: inquiry adapter, holder

---
+++ app/src/main/java/com/example/customer_inquiry_system_mobile/adapter/InquiryAdapter.java
@@ -0,0 +1,55 @@
+package com.example.customer_inquiry_system_mobile.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.customer_inquiry_system_mobile.Inquiry;
+import com.example.customer_inquiry_system_mobile.InquiryDetailActivity;
+import com.example.customer_inquiry_system_mobile.R;
+
+import java.util.List;
+
+public class InquiryAdapter extends RecyclerView.Adapter<InquiryHolder> {
+
+    private final List<Inquiry> inquiryList;
+    private final Context context;
+
+    public InquiryAdapter(List<Inquiry> inquiryList, Context context) {
+        this.inquiryList = inquiryList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public InquiryHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_inquiry_item, parent, false);
+        return new InquiryHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InquiryHolder holder, int position) {
+        Inquiry inquiry=inquiryList.get(position); //
+        holder.inquiryType.setText(inquiry.getInquiryType());
+        holder.progress.setText(inquiry.getProgress());
+        holder.customerName.setText(inquiry.getCustomerName());
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, InquiryDetailActivity.class);
+
+            intent.putExtra("inquiry_id", inquiry.getInquiryId());
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return inquiryList.size();
+    }
+}
---
+++ app/src/main/java/com/example/customer_inquiry_system_mobile/adapter/InquiryHolder.java
@@ -0,0 +1,21 @@
+package com.example.customer_inquiry_system_mobile.adapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.customer_inquiry_system_mobile.R;
+
+public class InquiryHolder extends RecyclerView.ViewHolder {
+
+    TextView inquiryType, progress ,customerName;
+
+    public InquiryHolder(@NonNull View itemView){
+        super(itemView);
+        inquiryType=itemView.findViewById(R.id.inquiryListItem_InquiryType);
+        progress=itemView.findViewById(R.id.inquiryListItem_Progress);
+        customerName=itemView.findViewById(R.id.inquiryListItem_CustomerName);
+    }
+}
