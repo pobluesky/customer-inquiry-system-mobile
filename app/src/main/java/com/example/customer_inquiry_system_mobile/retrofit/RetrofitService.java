b844b2ea1e3e4e40c01e67d2c6d6fef1c4dddefc
born-A <ra_ari@naver.com>
2024-09-02 17:14:28 +0900

    pbs-321--feat: Retrofit Service

---
+++ app/src/main/java/com/example/customer_inquiry_system_mobile/retrofit/RetrofitService.java
@@ -0,0 +1,26 @@
+package com.example.customer_inquiry_system_mobile.retrofit;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitService {
+
+    private Retrofit retrofit;
+
+    public RetrofitService(){
+        initializeRetrofit();
+    }
+
+    private void initializeRetrofit(){
+        retrofit=new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080/")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+    }
+
+    public Retrofit getRetrofit(){
+        return retrofit;
+    }
+}
