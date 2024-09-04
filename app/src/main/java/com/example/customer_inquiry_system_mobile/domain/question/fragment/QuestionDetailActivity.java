d470fb4c7870b2bbb5f42feb22a62e11cb8fd96c
born-A <ra_ari@naver.com>
2024-09-04 10:03:43 +0900

    pbs-276--feat: question 상세 조회

---
+++ app/src/main/java/com/example/customer_inquiry_system_mobile/domain/question/fragment/QuestionDetailActivity.java
@@ -0,0 +1,79 @@
+package com.example.customer_inquiry_system_mobile.domain.question.fragment;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.customer_inquiry_system_mobile.R;
+import com.example.customer_inquiry_system_mobile.domain.question.api.QuestionAPI;
+import com.example.customer_inquiry_system_mobile.domain.question.dto.QuestionResponseDTO;
+import com.example.customer_inquiry_system_mobile.global.RetrofitService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class QuestionDetailActivity extends AppCompatActivity {
+
+    private TextView title ,status, type;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_detail);
+
+        type = findViewById(R.id.questionTypeDetail);
+        status = findViewById(R.id.statusDetail);
+        title = findViewById(R.id.titleDetail);
+
+        Long questionId = getIntent().getLongExtra("question_id", -1);
+
+        if (questionId != -1) {
+            fetchQuestionById(questionId);
+        } else {
+            Toast.makeText(
+                    this,
+                    "유효하지 않은 문의 ID입니다.",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+    }
+
+    private void fetchQuestionById(Long questionId) {
+        RetrofitService retrofitService = new RetrofitService();
+        QuestionAPI questionAPI = retrofitService.getRetrofit().create(QuestionAPI.class);
+
+        questionAPI.getQuestionById(questionId).enqueue(new Callback<QuestionResponseDTO>() {
+            @Override
+            public void onResponse(@NonNull Call<QuestionResponseDTO> call, @NonNull Response<QuestionResponseDTO> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    QuestionResponseDTO questionResponseDTO = response.body();
+
+                    type.setText(questionResponseDTO.getType());
+                    status.setText(questionResponseDTO.getStatus());
+                    title.setText(questionResponseDTO.getTitle());
+                } else {
+                    Toast.makeText(
+                            QuestionDetailActivity.this,
+                            "데이터를 가져오는 데 실패했습니다.",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<QuestionResponseDTO> call, @NonNull Throwable t) {
+                Toast.makeText(
+                        QuestionDetailActivity.this,
+                        "API 호출 실패: " + t.getMessage(),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/customer_inquiry_system_mobile/domain/question/fragment/QuestionFragment.java
@@ -0,0 +1,85 @@
+package com.example.customer_inquiry_system_mobile.domain.question.fragment;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.customer_inquiry_system_mobile.R;
+import com.example.customer_inquiry_system_mobile.domain.question.adapter.QuestionAdapter;
+import com.example.customer_inquiry_system_mobile.domain.question.api.QuestionAPI;
+import com.example.customer_inquiry_system_mobile.domain.question.dto.QuestionResponseDTO;
+import com.example.customer_inquiry_system_mobile.global.RetrofitService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class QuestionFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    public QuestionFragment() {
+    }
+
+    public static QuestionFragment newInstance() {
+        return new QuestionFragment();
+    }
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_question, container,false);
+
+        recyclerView = rootView.findViewById(R.id.questionList_recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        loadQuestions();
+
+        return rootView;
+    }
+
+    private void loadQuestions() {
+        RetrofitService retrofitService = new RetrofitService();
+        QuestionAPI questionAPI = retrofitService.getRetrofit().create(QuestionAPI.class);
+
+        questionAPI.getAllQuestions().enqueue(new Callback<List<QuestionResponseDTO>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<QuestionResponseDTO>> call, @NonNull Response<List<QuestionResponseDTO>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    populateListView(response.body());
+                } else {
+                    Toast.makeText(
+                            getContext(),
+                            "Failed to load questions: " + response.message(),
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<QuestionResponseDTO>> call, @NonNull Throwable t) {
+                Toast.makeText(
+                        getContext(),
+                        "Failed to load questions",
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+        });
+    }
+
+    private void populateListView(List<QuestionResponseDTO> questionResponseDTOList) {
+        QuestionAdapter questionAdapter = new QuestionAdapter(questionResponseDTOList, getContext());
+        recyclerView.setAdapter(questionAdapter);
+    }
+}
