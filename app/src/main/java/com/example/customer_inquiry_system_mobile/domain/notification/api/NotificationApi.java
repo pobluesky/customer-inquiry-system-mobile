54b8f88ac102fa0fa27e9872e9cd9cd0bcf0f805
born-A <ra_ari@naver.com>
2024-09-09 17:43:06 +0900

    pbs-361--feat : 모바일 알림 읽은 / 안읽은 화면 구현

---
+++ app/src/main/java/com/example/customer_inquiry_system_mobile/domain/notification/api/NotificationApi.java
@@ -0,0 +1,19 @@
+package com.example.customer_inquiry_system_mobile.domain.notification.api;
+
+import com.example.customer_inquiry_system_mobile.domain.notification.dto.NotificationResponseDTO;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface NotificationApi {
+
+    @GET("mobile/api/notifications/{userId}")
+    Call<List<NotificationResponseDTO>> getUnreadNotifications(@Path("userId") Long userId);
+
+    @GET("mobile/api/notifications/read/{userId}")
+    Call<List<NotificationResponseDTO>> getReadNotifications(@Path("userId") Long userId);
+}
+
