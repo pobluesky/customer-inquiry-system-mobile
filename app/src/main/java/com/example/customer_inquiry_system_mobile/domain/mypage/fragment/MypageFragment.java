59a1e905c909b170836677404e2e3a6db2efb323
heejinssss <heejin.dev33@gmail.com>
2024-09-12 23:32:47 +0900

    pbs-363--feat: 대시보드 API 연동

--- app/src/main/java/com/example/customer_inquiry_system_mobile/domain/mypage/fragment/MypageFragment.java
+++ app/src/main/java/com/example/customer_inquiry_system_mobile/domain/mypage/fragment/MypageFragment.java
@@ -1,6 +1,7 @@
 package com.example.customer_inquiry_system_mobile.domain.mypage.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
