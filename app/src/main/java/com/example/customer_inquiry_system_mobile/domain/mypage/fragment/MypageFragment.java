d98f2dde1dc4c21061e834ddad57f27ad9a15725
heejinssss <heejin.dev33@gmail.com>
2024-09-15 01:49:32 +0900

    pbs-363--refactor: import 문 삭제

--- app/src/main/java/com/example/customer_inquiry_system_mobile/domain/mypage/fragment/MypageFragment.java
+++ app/src/main/java/com/example/customer_inquiry_system_mobile/domain/mypage/fragment/MypageFragment.java
@@ -1,7 +1,6 @@
 package com.example.customer_inquiry_system_mobile.domain.mypage.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
